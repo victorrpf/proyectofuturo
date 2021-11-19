1339721c175399d5a01ddb7e4d7d2879fab1316f
victorrpf <victor.rpf@gmail.com>
2021-11-19 23:54:39 +0100

    primera

---
+++ src/proyectofuturo/Aplicacion.java
@@ -0,0 +1,19 @@
+package proyectofuturo;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+ 
+        System.out.println("Estudiante " + args[0] + " : ");
+        System.out.println(Aplicacion.veredicto(args[1], args[2]));
+    }    
+    static String veredicto(String valor1, String valor2) {    
+    if (valor1.equals(valor2)) {
+    return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+    }
+    if (Integer.valueOf(valor1) > Integer.valueOf(valor2)){
+    return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+    }
+    return "Ideal. Trabajas los contenidos en casa.";
+    
+    }
+}
